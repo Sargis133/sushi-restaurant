8384b1712483ba5adea861575f52611cf3cf67f8
Sargis <saqobabloyan@gmail.com>
2024-04-29 16:39:51 +0400

    change image src

--- components/templates/blog/config/blogItems.ts
+++ components/templates/blog/config/blogItems.ts
@@ -2,31 +2,31 @@ import type {I_BlogItems} from "~/models/pages/blog/interfaces";
 
 export const blogItems: I_BlogItems[] = [
     {
-        image: 'blog-1.png',
+        image: 'items/pages/blog-1.png',
         date: '24TH AUG 2023',
         title: 'How Qitchen Redefines Flavor Harmony in Every Bite',
         text: 'Experience an orchestra of tastes as Qitchen\'s sushi unveils a symphony of perfectly balanced flavors.'
     },
     {
-        image: 'blog-2.png',
+        image: 'items/pages/blog-2.png',
         date: '24TH AUG 2023',
         title: 'Unveiling the Mastery Behind Our Culinary Craftsmanship',
         text: 'Explore the meticulous artistry and dedication that create Qitchen\'s renowned sushi perfection.'
     },
     {
-        image: 'blog-3.png',
+        image: 'items/pages/blog-3.png',
         date: '24TH AUG 2023',
         title: 'Journey through Freshness Exquisite Sushi Selection',
         text: 'Embark on a seafood adventure, guided by Qitchen\'s fresh and exquisite sushi creations from the sea.'
     },
     {
-        image: 'blog-4.png',
+        image: 'items/pages/blog-4.png',
         date: '24TH AUG 2023',
         title: 'Palate with Qitchen\'s Unsurpassed Sushi Delicacies',
         text: 'Discover the heights of gastronomic delight as Qitchen\'s sushi transports you to a new culinary realm.'
     },
     {
-        image: 'blog-5.png',
+        image: 'items/pages/blog-5.png',
         date: '24TH AUG 2023',
         title: 'The Qitchen Experience Beyond Sushi',
         text: 'Immerse in Qitchen\'s passion for culinary excellence, where sushi is more than a dish—it\'s an experience.'
