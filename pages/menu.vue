5287090dbb45438401a04b2a5d0a3bba14f5ff34
Sargis <saqobabloyan@gmail.com>
2024-04-29 14:58:53 +0400

    change images src

--- pages/menu.vue
+++ pages/menu.vue
@@ -27,7 +27,7 @@
             <div class="product-items__product-list">
               <div v-for="item in product.products" class="product-list__item">
                 <div class="item-image">
-                  <img :src="'/images/products/' + item.image" alt="product" />
+                  <img :src="'@/assets/images/products/' + item.image" alt="product" />
                 </div>
                 <div class="item-text">
                   <div class="item-text__title">
